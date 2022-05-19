3954c914e6d1d0fe6b6f397afb04dc690ac9eb34
zlt995413176 <995413176@qq.com>
2022-05-19 17:11:04 +0800

    Update BdInviteService.java

--- dool-biz/src/main/java/com/dool/biz/service/BdInviteService.java
+++ dool-biz/src/main/java/com/dool/biz/service/BdInviteService.java
@@ -4,10 +4,13 @@ import com.dool.biz.dao.entity.BdInvite;
 import java.util.List;
 
 /**
+
  * @author zenglt
  * @date 2020/4/17 16:24
  */
 public interface BdInviteService {
 
+    
     List<BdInvite> list();
+    
 }
